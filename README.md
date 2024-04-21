27c7af16717a6ddf9ed01cf9d2401326f965872b
JeahaOh <jeaha1217@gmail.com>
2024-04-21 22:10:08 +0900

    .

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # vue-date-calculator
+
 vue를 이용하여 날짜 계산기를 만들어 본다.
+
+
+## 참고
+
+- https://vue3datepicker.com/
+- https://kr.calc-date.com/
+- https://support.lawtop.co.kr/period_renew/index_all.asp
+- https://superkts.com/cal/days/
+- https://picocss.com/
---
+++ date-calculator/src/components/HelloWorld.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="hello">
+    <VueDatePicker
+      locale="ko"
+      v-model="date"
+      :enable-time-picker="false"
+      :esc-close="true"
+      :space-confirm="true"
+      arrow-navigation
+      cancel-text="Close"
+      text-input
+    />
+      : format="format"
+  </div>
+</template>
+
+<script setup>
+import {ref} from 'vue';
+import VueDatePicker from '@vuepic/vue-datepicker';
+import '@vuepic/vue-datepicker/dist/main.css'
+
+const date = ref(new Date());
+// const format = (date) => {
+//   const YYYY = date.getFullYear();
+//   let MM = date.getMonth() + 1;
+//   MM = MM < 10 ? '0' + MM : MM;
+//   let DD = date.getDate();
+//   DD = DD < 10 ? '0' + DD : DD;
+
+//   return `${YYYY}.${MM}.${DD}`;
+// }
+
+// const onKeypress = (date) => {
+//   return date;
+// }
+</script>
