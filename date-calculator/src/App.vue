5da83f05aec89c34ae39546e09119b768c9e09c9
JeahaOh <jeaha1217@gmail.com>
2024-05-06 14:03:17 +0900

    edit : TODO

--- date-calculator/src/App.vue
+++ date-calculator/src/App.vue
@@ -2,11 +2,11 @@
   <div>
     <AppHeader/>
     <!--
-      <DDayCalculator/>
       <PeriodCalculator/>
       <DateAddCalculator/>
+      <DDayCalculator/>
+      <AnniversaryCalculator/>
     -->
-    <AnniversaryCalculator/>
     <!-- <AppFooter/> -->
   </div>
 </template>
@@ -16,17 +16,17 @@ import AppHeader from './components/common/AppHeader.vue';
 // import PeriodCalculator from './components/PeriodCalculator.vue';
 // import DateAddCalculator from './components/DateAddCalculator.vue';
 // import DDayCalculator from './components/DDayCalculator.vue';
+// import AnniversaryCalculator from './components/AnniversaryCalculator.vue';
 // import AppFooter from './components/common/AppFooter.vue';
-import AnniversaryCalculator from './components/AnniversaryCalculator.vue';
 
 export default {
   name: 'App',
   components: {
     AppHeader,
-    AnniversaryCalculator
     // PeriodCalculator,
     // DateAddCalculator,
     // DDayCalculator,
+    // AnniversaryCalculator
     // , AppFooter
   }
 }
